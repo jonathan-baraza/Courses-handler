6bc8149803f951f062f4f605b9f34ada8f818208
jonathan_baraza <jbaraza46@gmail.com>
2023-04-30 11:01:24 +0300

    fix: team page layout

--- app/about/team/page.tsx
+++ app/about/team/page.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const AboutTeam = () => {
   return (
-    <div>
-      <h1>About our team</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut incidunt accusamus alias reiciendis reprehenderit sequi odit unde vitae architecto vel, minus minima, itaque laudantium necessitatibus, rerum eveniet. Ea, minus inventore?</p>
+    <div className="P-4">
+      <h1 className="font-bold my-4 text-2xl">About our team</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut incidunt
+        accusamus alias reiciendis reprehenderit sequi odit unde vitae
+        architecto vel, minus minima, itaque laudantium necessitatibus, rerum
+        eveniet. Ea, minus inventore?
+      </p>
     </div>
   );
 };
